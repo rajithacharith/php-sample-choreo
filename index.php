ba90cbfcdf5f4d2914d72b969ce581fbcfd4bb6f
rajithacharith <rajithacharith@gmail.com>
2023-07-31 11:53:00 +0530

    Decode jwt

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ require_once 'config.php';
 // check header X-Forwarded-Access-Token.
 if (isset($_SERVER['HTTP_X_FORWARDED_ACCESS_TOKEN'])) { 
   echo "X-Forwarded-Access-Token: " . $_SERVER['HTTP_X_FORWARDED_ACCESS_TOKEN'];
+  $token=$_SERVER['HTTP_X_FORWARDED_ACCESS_TOKEN'];
+  print_r(json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $token)[1])))));
 }
 
 ?>
