518fe20d8200b351898718a169902ee3c40371ff
rajithacharith <rajithacharith@gmail.com>
2023-07-31 11:56:28 +0530

    Add welcome message

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ require_once 'config.php';
 if (isset($_SERVER['HTTP_X_FORWARDED_ACCESS_TOKEN'])) { 
   echo "X-Forwarded-Access-Token: " . $_SERVER['HTTP_X_FORWARDED_ACCESS_TOKEN'];
   $token=$_SERVER['HTTP_X_FORWARDED_ACCESS_TOKEN'];
-  print_r(json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $token)[1])))));
+  $token_data= json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $token)[1]))));
+  echo "Hello " . $token_data->email;
 }
 
 ?>
