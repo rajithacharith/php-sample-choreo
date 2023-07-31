26b45a705877dbd8fb607736de652519a78c53f8
rajithacharith <rajithacharith@gmail.com>
2023-07-31 13:05:05 +0530

    Remove echo

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ require_once 'config.php';
 
 // check header X-Forwarded-Access-Token.
 if (isset($_SERVER['HTTP_X_FORWARDED_ACCESS_TOKEN'])) { 
-  echo "X-Forwarded-Access-Token: " . $_SERVER['HTTP_X_FORWARDED_ACCESS_TOKEN'];
   $token=$_SERVER['HTTP_X_FORWARDED_ACCESS_TOKEN'];
   $token_data= json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $token)[1]))));
   echo "Hello " . $token_data->email;
