9ba0e7f2b6eeda59db757dc8964acd88f30069be
rajithacharith <rajithacharith@gmail.com>
2023-07-31 14:23:34 +0530

    Add welcome page support

--- welcome.php
+++ welcome.php
@@ -1,33 +1,10 @@
 <?php
-
-require_once 'config.php';
-
-// authenticate code from Google OAuth Flow
-if (isset($_GET['code'])) {
-    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-    $client->setAccessToken($token['access_token']);
-  
-    // get profile info
-    $google_oauth = new Google_Service_Oauth2($client);
-    $google_account_info = $google_oauth->userinfo->get();
-    $userinfo = [
-      'email' => $google_account_info['email'],
-      'first_name' => $google_account_info['givenName'],
-      'last_name' => $google_account_info['familyName'],
-      'gender' => $google_account_info['gender'],
-      'full_name' => $google_account_info['name'],
-      'picture' => $google_account_info['picture'],
-      'verifiedEmail' => $google_account_info['verifiedEmail'],
-      'token' => $google_account_info['id'],
-    ];
-    
-    foreach ($userinfo as $key => $value) {
-        $_SESSION[$key] = $value;
-    }
-    echo $_SESSION['email'];
-} else {
-    $username = $_SESSION['email'];
-    echo "Hi, $username.";
-    echo "<br><a href='logout.php'>Logout</a>";
-}
+// check header X-Forwarded-Access-Token.
+if (isset($_SERVER['HTTP_X_FORWARDED_ACCESS_TOKEN'])) { 
+    $token=$_SERVER['HTTP_X_FORWARDED_ACCESS_TOKEN'];
+    $token_data= json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $token)[1]))));
+    echo "Welcome to Demo application";
+    echo "<br>";
+    echo "Your email: " . $token_data->email;
+  }
 ?>
