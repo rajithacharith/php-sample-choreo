d56c43e6eaa05621ee73543fe1832c45b08334fe
rajithacharith <rajithacharith@gmail.com>
2023-08-03 16:14:09 +0530

    Add space for auth token

--- welcome.php
+++ welcome.php
@@ -14,7 +14,7 @@ if (isset($_SERVER['HTTP_X_FORWARDED_ACCESS_TOKEN'])) {
     $url = "https://a0a51ad4-1acd-4ff5-9185-db759f540c40-dev.e1-us-east-azure.choreoapis.dev/yluh/expressbackend/product-catalog-803/1.0.0/products";
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-      'Authorization: Bearer' . $token
+      'Authorization: Bearer ' . $token
     ));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($ch);
