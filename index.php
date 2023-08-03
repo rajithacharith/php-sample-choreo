f3223ad619de8449e93795e29df052c953cd27fe
rajithacharith <rajithacharith@gmail.com>
2023-08-03 16:08:05 +0530

    Add curl command

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ require_once 'config.php';
 // check header X-Forwarded-Access-Token.
 if (isset($_SERVER['HTTP_X_FORWARDED_ACCESS_TOKEN'])) { 
   $token=$_SERVER['HTTP_X_FORWARDED_ACCESS_TOKEN'];
-  echo $token;
   $token_data= json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $token)[1]))));
   echo "Hello " . $token_data->email;
 }
--- welcome.php
+++ welcome.php
@@ -6,5 +6,14 @@ if (isset($_SERVER['HTTP_X_FORWARDED_ACCESS_TOKEN'])) {
     echo "Welcome to Demo application";
     echo "<br>";
     echo "Your email: " . $token_data->email;
+
+    echo "<br>";
+    echo "=================================================================== <br>";
+    // call to another API
+    $url = "https://a0a51ad4-1acd-4ff5-9185-db759f540c40-dev.e1-us-east-azure.choreoapis.dev/yluh/expressbackend/product-catalog-803/1.0.0/products";
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    echo "Response from another API: " . $response;
   }
 ?>
