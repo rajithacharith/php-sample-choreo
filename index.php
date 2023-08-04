7ca673581c5fcc9c69a19520c19e70359f23e953
rajithacharith <rajithacharith@gmail.com>
2023-08-04 11:34:14 +0530

    check authorization

--- index.php
+++ index.php
@@ -9,13 +9,10 @@ if (isset($_SERVER['HTTP_X_FORWARDED_ACCESS_TOKEN'])) {
   echo "Hello " . $token_data->email;
 }
 
-// check authorization header.
-if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
-  $token=$_SERVER['HTTP_AUTHORIZATION'];
-  echo "<br>";
-  echo $token;
-  $token_data= json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $token)[1]))));
-  echo "Hello " . $token_data->email;
+$headers = getallheaders();
+
+if (in_array('Authorization', $headers)) {
+    echo $headers['Authorization'];
 }
 
 ?>
